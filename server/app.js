68a9e4227054b260d9c58075c5e9262dbec303da
Aditya <you@example.com>
2020-01-10 12:50:29 +0530

    Second Commit

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://13.233.149.105:3000',
+    origin: 'http://13.233.149.105.xip.io:3000',
     credentials: true
 }));
 
